c5f90027bda6ce445b0f12f816b3c9d97507acc1
tejaspatil159 <117001605+tejaspatil159@users.noreply.github.com>
2024-01-13 15:14:55 +0530

    new commit

--- firstProgram.js
+++ firstProgram.js
@@ -2,3 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var greet = "Hello";
 console.log(greet);
+//-----------------------variable declartion------------------
+//let and var
+var a = 10;
+var b = 20;
+var sum;
+sum = a + b;
+console.log(sum);
+//const must be initiallized with value
+//const title;
+var title = "typescript";
+//-------------------------variable types----------------------
+//----------------1 boolean
+//----------------2 number
+//----------------3 string
+var isEnable = true;
+var pie = 3.14;
+var name = "Tejas";
+//--------Template string
+var identity = "I am ".concat(name, ", value of pie is ").concat(pie, " your value is ").concat(isEnable);
+console.log(identity);
--- firstProgram.ts
+++ firstProgram.ts
@@ -1,3 +1,31 @@
 export {};
 let greet = "Hello";
 console.log(greet);
+
+//-----------------------variable declartion------------------
+//let and var
+
+let a = 10;
+let b = 20;
+let sum;
+sum = a + b;
+console.log(sum);
+
+//const must be initiallized with value
+//const title;
+const title = "typescript";
+
+//-------------------------variable types----------------------
+
+//----------------1 boolean
+//----------------2 number
+//----------------3 string
+
+let isEnable: boolean = true;
+let pie: number = 3.14;
+const name: string = "Tejas";
+
+//--------Template string
+
+let identity = `I am ${name}, value of pie is ${pie} your value is ${isEnable}`;
+console.log(identity);
